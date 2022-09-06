7bfa41e31c4a25cd3686cb6936eec8110e0c9d37
Stanislav Stamatov <stanislav.stamatov45@abv.bg>
2022-09-06 18:55:14 +0300

    Snake game including skeleton and extended code

---
+++ Snake Game/SimpleSnake/GameObjects/Foods/FoodHeart.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleSnake.GameObjects.Foods
+{
+
+    using System;
+    
+    public class FoodHeart : Food
+    {
+        private const int foodPoints = 10;
+        private const char foodSymbol = '\u2665';
+        private const ConsoleColor foodColor = ConsoleColor.Magenta;
+
+        public FoodHeart(Field field)
+            : base(field, foodPoints, foodSymbol, foodColor)
+        {
+
+        }
+    }
+}
